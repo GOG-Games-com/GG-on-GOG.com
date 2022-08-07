3e4b95008fb30cd3df34fa82a1e2c1a59e5dd50b
Nailz776 <109116372+Nailz776@users.noreply.github.com>
2022-08-07 02:48:17 +0000

    Update gg-on-gog.js

--- gg-on-gog.js
+++ gg-on-gog.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name GG on GOG
-// @homepageURL https://gog-games.com
-// @description Add GOG DB and GOG Games links to the GOG store
+// @description Add a GOG Games link to the GOG store
 // @version 1.0
 // @author GOG Games, Yepoleb
 // @license CC0
@@ -13,26 +12,12 @@
 // @grant unsafeWindow
 // ==/UserScript==
 
-var product_id = unsafeWindow.productcardData.cardProductId;
 
-var product_slug = unsafeWindow.productcardData.cardProductSlug;
-
-var gogdb_element = document.createElement("a");
-gogdb_element.textContent = "GOGdb";
-gogdb_element.setAttribute("href", "https://www.gogdb.org/product/" + product_id);
-gogdb_element.className = "details__link";
-gogdb_element.setAttribute("target", "_blank");
-
-var separator_element = document.createTextNode(", ");
-
-var links_xpath = "//a[@class='details__link' and contains(@href, 'gog.com/forum')]";
-var links_element = document.evaluate(links_xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentNode;
-links_element.appendChild(separator_element);
-links_element.appendChild(gogdb_element);
+var cardProductSlug = unsafeWindow.productcardData.cardProductSlug;
 
 var gg_element = document.createElement("a");
-gg_element.textContent = "Download";
-gg_element.setAttribute("href", "https://gog-games.com/game/" + product_slug);
+gg_element.textContent = "Download from GG";
+gg_element.setAttribute("href", "https://gog-games.com/game/" + cardProductSlug);
 gg_element.className = "details__link";
 gg_element.setAttribute("target", "_blank");
 
